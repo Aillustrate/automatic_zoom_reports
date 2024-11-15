138aab771b510804d6922f158b80f4770ff855d4
anna-marshalova <missmarshal22@gmail.com>
2024-11-15 16:53:03 +0300

    add sound extraction from video

---
+++ utils.py
@@ -0,0 +1,9 @@
+from moviepy.editor import AudioFileClip
+
+def extract_audio(input_video_path, output_audio_path="output_audio.wav"):
+    audio_clip = AudioFileClip(input_video_path)
+    audio_clip.write_audiofile(output_audio_path)
+    audio_clip.close()
+
+    print(f"Audio saved as {output_audio_path}")
+    return output_audio_path
