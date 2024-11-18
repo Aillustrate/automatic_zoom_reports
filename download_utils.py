fe5092f4a2cbadb24c53c643e999c436f0104c01
anna-marshalova <missmarshal22@gmail.com>
2024-11-18 17:42:25 +0300

    fix encoding

--- download_utils.py
+++ download_utils.py
@@ -1,3 +1,4 @@
+import locale
 import yt_dlp
 
 def download_video_from_youtube(link, name='%(title)s'):
@@ -8,4 +9,5 @@ def download_video_from_youtube(link, name='%(title)s'):
         info_dict = ydl.extract_info(link, download=True)
         downloaded_file_path = ydl.prepare_filename(info_dict)
     print(f"Видео {downloaded_file_path} успешно загружено!")
+    locale.getpreferredencoding = lambda: "UTF-8"
     return downloaded_file_path
