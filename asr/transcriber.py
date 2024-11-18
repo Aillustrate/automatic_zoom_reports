739a9cc91cf16437e3532486e40fb2e86b65f101
anna-marshalova <missmarshal22@gmail.com>
2024-11-18 15:58:05 +0300

    add audio extraction and verbode mode

--- asr/transcriber.py
+++ asr/transcriber.py
@@ -2,6 +2,9 @@ import argparse
 from asr.diarization import Diarizer
 from asr.recognition import Recognizer
 from asr.transcription import Transcription
+from asr.utils import extract_audio
+
+from config import config
 
 
 class Transriber:
@@ -10,8 +13,8 @@ class Transriber:
         recognizer=None,
         diarizer=None,
         recognizer_model_dir=None,
-        language="ru",
-        diarizer_model="pyannote/speaker-diarization-3.1",
+        language=config.asr.language,
+        diarizer_model=config.asr.diarization_model
     ):
         if recognizer is None:
             self.recognizer = Recognizer(
@@ -24,16 +27,28 @@ class Transriber:
         else:
             self.diarizer = diarizer
 
-    def transcribe(self, input_path):
+    def transcribe(self, input_path, from_video=True, verbose=True):
+        VIDEO_FORMATS = ["mp4", "mkv", "avi"]
+        AUDIO_FORMATS = ["wav", "mp3", "ogg", "flac"]
+        if from_video and input_path.split(".")[-1] in VIDEO_FORMATS:
+            if verbose:
+                print("Extracting audio from video...")
+            input_path = extract_audio(input_path)
+        if verbose:
+            print("Recognizing audio...")
         texts_with_timestamps = self.recognizer.recognize(input_path)
+        if verbose:
+            print("Identifying speakers...")
         diarization = self.diarizer.diarize(input_path)
         return Transcription(texts_with_timestamps, diarization)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_path", type=str, help="Path to input audio file")
+    parser.add_argument("--input_path", type=str, help="Path to input audio file")
+    parser.add_argument("--from-video", action="store_true", help="Whether to transcribe from video or audio file")
+    parser.add_argument("--verbose", action="store_true", help="Whether to print detailed logs")
     args = parser.parse_args()
     transriber = Transriber()
-    transcription = transriber.transcribe(args.input_path)
+    transcription = transriber.transcribe(args.input_path, from_video=args.from_video, verbose=args.verbose)
     print(transcription)
