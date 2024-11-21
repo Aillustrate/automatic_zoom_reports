957634357cb27d25a6abaeacd6a8296fedfa8d61
anna-marshalova <missmarshal22@gmail.com>
2024-11-21 20:05:54 +0300

    specify model in report

--- llm/llm.py
+++ llm/llm.py
@@ -14,12 +14,14 @@ class LLM:
             system_prompt_path = os.path.join(config.llm.prompts_dir, system_prompt_file)
             with open(system_prompt_path, "r") as f:
                 system_prompt = f.read()
-        self.token_usage_report_path = token_usage_report_path
-        with open(self.token_usage_report_path, "r") as f:
-            self.token_usage_report = json.load(f)
         self.system_prompt = system_prompt
         self.model_name = model_name
         self.client = OpenAI(api_key=config.auth.openai_api_key)
+        self.token_usage_report_path = token_usage_report_path
+        with open(self.token_usage_report_path, "r") as f:
+            self.token_usage_report = json.load(f)
+        if self.model_name not in self.token_usage_report:
+            self.token_usage_report[self.model_name] = {"total_prompt_tokens": 0, "total_completion_tokens": 0}
         
     
     def get_response(self, prompt):
@@ -36,14 +38,14 @@ class LLM:
     def report_token_usage(self, response):
         prompt_tokens = response.usage.prompt_tokens
         completion_tokens = response.usage.completion_tokens
-        self.token_usage_report["total_prompt_tokens"] += prompt_tokens
-        self.token_usage_report["total_completion_tokens"] += completion_tokens
-        with open(config.llm.token_usage_report_path, "w") as f:
+        self.token_usage_report[self.model_name]["total_prompt_tokens"] += prompt_tokens
+        self.token_usage_report[self.model_name]["total_completion_tokens"] += completion_tokens
+        with open(self.token_usage_report_path, "w") as f:
             json.dump(self.token_usage_report, f)
             
     def show_token_usage_report(self):
-        prompt_tokens = self.token_usage_report["total_prompt_tokens"]
-        completion_tokens = self.token_usage_report["total_completion_tokens"]
+        prompt_tokens = self.token_usage_report[self.model_name]["total_prompt_tokens"]
+        completion_tokens = self.token_usage_report[self.model_name]["total_completion_tokens"]
         print(f"Total prompt tokens: {prompt_tokens}")
         print(f"Total completion tokens: {completion_tokens}")
         print(f"Total tokens: {prompt_tokens + completion_tokens}")
