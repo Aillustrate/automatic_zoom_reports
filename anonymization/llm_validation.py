06e836473be7b349365c6ac151bd3d985bd3c324
anna-marshalova <missmarshal22@gmail.com>
2025-03-01 00:10:44 +0300

    update llm validation

--- anonymization/llm_validation.py
+++ anonymization/llm_validation.py
@@ -38,12 +38,23 @@ class LLMValidator:
         self.logprobs = logprobs
         
 
-    def get_prompt(self, context, entity):
-        return f"""
-        Ответь YES, если сущность явялется конфиденциальной информацией и явялется именем собственным, NO - если сущность НЕ явялется конфиденциальной информацией или не является именем собственным.
+    def get_prompt(self, entity, context):
+        if "<money>" in entity:
+            prefix = "Ответь YES, если сущность содержит денежную сумму, NO - иначе."
+        if "<person>" in entity:
+            prefix = "Ответь YES, если сущность содержит указания на личность человека (например, имя, фамилию или отчество), NO - иначе."
+        if "<address>" in entity:
+            prefix = "Ответь YES, если сущность содержит адрес (например, город, район, улица или номер дома), NO - иначе."
+        if "<org>" in entity:
+            prefix = "Ответь YES, если сущность содержит название организации, NO - иначе."
+        else:
+            prefix = "Ответь YES, если сущность является конфиденциальной информацией и явялется именем собственным, NO - если сущность НЕ является конфиденциальной информацией или не является именем собственным."
+        prompt = f"""{prefix}
         КОНТЕКСТ: {context}
         СУЩНОСТЬ: {entity}
         ОТВЕТ:"""
+        print(prompt)
+        return prompt
     
     def parse_responses(self, responses): #try logprobs
         verdicts = []
@@ -57,8 +68,8 @@ class LLMValidator:
     def judge(self, entities, contexts):
         prompts = [
             self.get_prompt(entity, context)
-            for entity, context in zip(entities, contexts)]
-        print(entities)
+            for ut_entities, context in zip(entities, contexts)
+            for entity in ut_entities]
         if self.logprobs:
             logprobs = self.llm.get_logprobs(prompts)
             verdicts = get_verdicts(logprobs)
@@ -76,13 +87,16 @@ class LLMValidator:
             contexts.append(context)
             entities.append([])
             positions = get_entity_positions(ut_labels)
-            entity_positions.append(positions)
+            filtered_postitions = []
             if positions:
                 for start, end in positions:
                     entity = bio2tag(ut_tokens[start:end], ut_labels[start:end])
                     if not hasproper(entity) and not hasnum(entity):
                         entities[-1].append((entity))
+                        filtered_postitions.append((start, end))
+            entity_positions.append(filtered_postitions)
         return entities, contexts, entity_positions
+
     
     def validate_entities(self, tokens, labels):
         entities, contexts, entity_positions = self.get_entities(tokens, labels)
