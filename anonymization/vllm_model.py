eb49254ccb2b404dbee4249a4ff0ff29ed1978dc
anna-marshalova <missmarshal22@gmail.com>
2025-03-14 20:14:04 +0300

    fix llm loading

--- anonymization/vllm_model.py
+++ anonymization/vllm_model.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer
 
 
 def load_vllm_and_tokenizer(model_name_or_path):
-    model = LLM.load(model_name_or_path)
+    model = LLM(model_name_or_path)
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     return model, tokenizer
 
